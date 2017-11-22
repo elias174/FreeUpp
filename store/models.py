3478fef2cb6c00e6afd8870dae6eb33774dde41c
Elias Valencia <elias.valencia@ucsp.edu.pe>
2017-11-22 18:03:05 -0500

    fixing 4 spaces

--- store/models.py
+++ store/models.py
@@ -7,17 +7,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import URLValidator
 
+
 # Create your models here.
 
 class Category(models.Model):
-	name = models.CharField(max_length=140)
+    name = models.CharField(max_length=140)
+
 
 class Product(models.Model):
-	title  = models.CharField(max_length=250)
-	description = models.TextField()
-	date = models.TextField()
-	company = models.ForeignKey(User, null=True)
-	location = models.CharField(max_length=400)
-	code  = models.CharField(max_length=250)
-	category = models.ForeignKey(Category, null=True)
-	image_url = models.TextField(validators=[URLValidator()], blank=True, null=True)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    date = models.TextField()
+    company = models.ForeignKey(User, null=True)
+    location = models.CharField(max_length=400)
+    code = models.CharField(max_length=250)
+    category = models.ForeignKey(Category, null=True)
+    image_url = models.TextField(validators=[URLValidator()], blank=True,
+                                 null=True)
